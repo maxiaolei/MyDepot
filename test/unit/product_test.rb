fc5920bac7868644ab65eca301bdc514991ebf23
maxiaolei <1049913552@qq.com>
2011-07-20 17:33:46 +0800

    add something

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -49,4 +49,14 @@ class ProductTest < ActiveSupport::TestCase
      end
   end
   
+  test "product is not valid without a unique title" do
+     product = Product.new(:title => products(:ruby).title,
+             :description => "yyy",
+             :price => 1,
+            :image_url => "fred.gif")
+            
+     assert !product.save
+     assert_equal "has already been taken",product.errors[:title].join('; ')
+  end
+  
 end
